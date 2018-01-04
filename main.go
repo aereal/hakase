1491243b2a1e0371344bb194b2691b1c8deeb05a
aereal <aereal@aereal.org>
2018-01-04 15:35:52 +0900

    Introduce scanner

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"os"
 	"runtime"
-	"sync"
 )
 
 type arguments struct {
@@ -38,45 +37,13 @@ func main() {
 	}
 
 	cpus := runtime.NumCPU()
-	procs := cpus * 2
-	res := run(procs, args)
+	scanner := newConcurrentRepoScanner(cpus*2, args.maxCommits)
+	res := scanner.scan(args.repoPath, args.files)
 	jsonRet, err := json.Marshal(res)
 	if err != nil {
 		log.Fatalf("error: %s", err)
 	}
 	fmt.Fprintf(os.Stdout, string(jsonRet))
-}
-
-func run(maxConcurrency int, args *arguments) surveyResult {
-	semaphore := make(chan int, maxConcurrency)
-	ch := make(chan *fileSurveyResult, len(args.files))
-	var wg sync.WaitGroup
-	go func(ch chan *fileSurveyResult) {
-		for _, argFile := range args.files {
-			wg.Add(1)
-			go func(ch chan *fileSurveyResult, file string) {
-				defer wg.Done()
-				semaphore <- 1
-				ret, err := scanFile(args.repoPath, file, args.maxCommits)
-				fileRet := &fileSurveyResult{file: file, result: ret}
-				if err != nil {
-					log.Printf("error: %s", err)
-				}
-				ch <- fileRet
-				<-semaphore
-			}(ch, argFile)
-		}
-		wg.Wait()
-		close(ch)
-	}(ch)
-
-	res := surveyResult{
-		Files: map[string]ownershipSurvey{},
-	}
-	for fileRet := range ch {
-		res.Files[fileRet.file] = fileRet.result
-	}
-	return res
 }
 
 func parseArgs() (*arguments, error) {
--- scanner.go
+++ scanner.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"log"
 	"os/exec"
+	"sync"
 )
 
 type ownershipScore struct {
@@ -23,9 +25,58 @@ type surveyResult struct {
 	Files map[string]ownershipSurvey `json:"files"`
 }
 
-func scanFile(repoPath string, file string, maxCommits int) (ownershipSurvey, error) {
+type repoScanner interface {
+	scan(repo string, files filesList) surveyResult
+}
+
+type concurrentRepoScanner struct {
+	maxConcurrency int
+	maxCommits     int
+}
+
+func newConcurrentRepoScanner(maxConcurrency int, maxCommits int) concurrentRepoScanner {
+	scanner := concurrentRepoScanner{
+		maxConcurrency: maxConcurrency,
+		maxCommits:     maxCommits,
+	}
+	return scanner
+}
+
+func (s concurrentRepoScanner) scan(repo string, files filesList) surveyResult {
+	semaphore := make(chan int, s.maxConcurrency)
+	ch := make(chan *fileSurveyResult, len(files))
+	var wg sync.WaitGroup
+	go func(ch chan *fileSurveyResult) {
+		for _, argFile := range files {
+			wg.Add(1)
+			go func(ch chan *fileSurveyResult, file string) {
+				defer wg.Done()
+				semaphore <- 1
+				ret, err := s.scanFile(repo, file)
+				fileRet := &fileSurveyResult{file: file, result: ret}
+				if err != nil {
+					log.Printf("error: %s", err)
+				}
+				ch <- fileRet
+				<-semaphore
+			}(ch, argFile)
+		}
+		wg.Wait()
+		close(ch)
+	}(ch)
+
+	res := surveyResult{
+		Files: map[string]ownershipSurvey{},
+	}
+	for fileRet := range ch {
+		res.Files[fileRet.file] = fileRet.result
+	}
+	return res
+}
+
+func (s concurrentRepoScanner) scanFile(repoPath string, file string) (ownershipSurvey, error) {
 	outBuf := new(bytes.Buffer)
-	cmd := exec.Command("git", "-C", repoPath, "log", "-C", "-M", "--no-merges", "-n", fmt.Sprintf("%d", maxCommits), "--format=%aN", file)
+	cmd := exec.Command("git", "-C", repoPath, "log", "-C", "-M", "--no-merges", "-n", fmt.Sprintf("%d", s.maxCommits), "--format=%aN", file)
 	cmd.Stdout = outBuf
 	err := cmd.Run()
 	if err != nil {
